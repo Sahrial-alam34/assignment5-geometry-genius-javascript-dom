7f1eb413784ad3797c779fd7e297e4d8aa29a7c7
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-02-18 11:33:52 +0600

    first card calulation using function

--- js/calculation.js
+++ js/calculation.js
@@ -1,9 +1,28 @@
+let serial = 0;
 // first card section
 document.getElementById('first-card-color').addEventListener('mouseover', function () {
 
-    
+
     // for change color
     colorCardSection('first-card-color');
-  
 
- })
+
+})
+
+//  second card section
+
+document.getElementById('second-card-butt').addEventListener('click', function () {
+    serial++;
+    const element = getElement('name-rectangle','weight-rectangle' , 'length-rectangle' );
+   
+
+    const totalArea = (parseFloat(element.elementHeight) * parseFloat(element.elementWeight)).toFixed(2);
+    //console.log(totalArea);
+    displayList(element.elementName, totalArea);
+    forEmptyBox('weight-rectangle');
+    forEmptyBox('length-rectangle');
+
+
+})
+
+
--- js/utilities.js
+++ js/utilities.js
@@ -2,6 +2,49 @@ document.getElementById('nav-button').addEventListener('click', function () {
     window.location.href = 'question.html'
 })
 
+
+// get element from html
+function getElement(eleName,eleWeight, eleHeight){
+    const elementName = document.getElementById(eleName).innerText;
+    const elementWeight = document.getElementById(eleWeight).value;
+    const elementHeight = document.getElementById(eleHeight).value;
+
+    const element = {
+        elementName : elementName,
+        elementWeight : elementWeight,
+        elementHeight : elementHeight,
+
+    }
+    return element
+}
+
+// display the list
+function displayList(elementName, totalArea) {
+    const container = document.getElementById("table-container");
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td>${serial}</td>
+    <td>${elementName}</td>
+    <td>${totalArea}</td>
+    <td>cm<sup>2</sup></td>
+    
+   
+    <td>
+    <button class="btn btn-primary btn-sm ">Covert to m<sup>2</sup></button>
+    </td>
+    
+  `;
+    container.appendChild(tr);
+  
+}
+
+
+// for empty text box
+
+function forEmptyBox(id){
+    document.getElementById(id).value = '';
+}
+
 function colorCardSection(id){
     const colorCard = document.getElementById(id);
     let symbols = '0123456789ABCDEF' //16 symbols
